ae32c4641bc5874553db0bb8ba0cb5d1775dcf36
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-13 19:02:25 -0300

    Implementar as validações na classe Abstrata Entidades e sobrepor metodo na classe planeta

--- StarWarPlanets/StarWarPlanets.Domain/Entidades/Entidade.cs
+++ StarWarPlanets/StarWarPlanets.Domain/Entidades/Entidade.cs
@@ -1,7 +1,33 @@
-﻿namespace StarWarPlanets.Domain.Entidades
-{
-   public abstract class Entidade
-    {
+﻿using System.Collections.Generic;
+using System.Linq;
 
+namespace StarWarPlanets.Domain.Entidades
+{
+    public abstract class Entidade
+    {
+        public List<string> _mensagensValidacao { get; set; }
+        public List<string> mensagemValidacao
+        {
+
+            get { return _mensagensValidacao ?? (_mensagensValidacao = new List<string>()); }
+
+        }
+
+        protected void LimparMensagemValidacao()
+        {
+            mensagemValidacao.Clear();
+        }
+
+        protected void AdicionarCritica(string mensagem)
+        {
+            mensagemValidacao.Add(mensagem);
+        }
+
+        public abstract void Validate();
+
+        protected bool EhValido
+        {
+            get { return !mensagemValidacao.Any(); }
+        }
     }
 }
--- StarWarPlanets/StarWarPlanets.Domain/Entidades/Planeta.cs
+++ StarWarPlanets/StarWarPlanets.Domain/Entidades/Planeta.cs
@@ -1,6 +1,6 @@
 ﻿namespace StarWarPlanets.Domain.Entidades
 {
-    public class Planeta
+    public class Planeta: Entidade
     {
         public int Id { get; set; }
         public string Nome { get; set; }
@@ -8,5 +8,27 @@
         public string Terreno { get; set; }
         public int QtdAparicao { get; set; }
 
+        public override void Validate()
+        {
+            LimparMensagemValidacao();
+            if(string.IsNullOrEmpty(Nome))
+            {
+                AdicionarCritica("Crítica - Nome deve estar preenchido.");
+            }
+            if(string.IsNullOrEmpty(Clima))
+            {
+                AdicionarCritica("Crítica - Clima deve estar preenchido.");
+            }
+
+            if (string.IsNullOrEmpty(Terreno))
+            {
+                AdicionarCritica("Crítica - Terreno deve estar preenchido.");
+            }
+
+            if (QtdAparicao < 0)
+            {
+                AdicionarCritica("Crítica - Quantidade de Aparições não pode ser menor que zero.");
+            }
+        }
     }
 }
