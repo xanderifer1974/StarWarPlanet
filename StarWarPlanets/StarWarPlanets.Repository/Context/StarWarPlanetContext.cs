493eee465b90f47389ce8db9482f98aeed375cc7
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-14 11:13:38 -0300

    Criacao do DbContex e configuracao do DbSet

---
+++ StarWarPlanets/StarWarPlanets.Repository/Context/StarWarPlanetContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StarWarPlanets.Domain.Entidades;
+
+namespace StarWarPlanets.Repository.Context
+{
+    public class StarWarPlanetContext: DbContext
+    {
+        public DbSet<Planeta> Planetas { get; set; }
+
+    }
+}
