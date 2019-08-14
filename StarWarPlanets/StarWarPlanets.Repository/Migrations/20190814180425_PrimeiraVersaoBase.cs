64f17376ac09f4e7192b2506ec02fab20f66ed09
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-14 15:10:50 -0300

    Criacao da primeira versao da base de dados com o Migration

---
+++ StarWarPlanets/StarWarPlanets.Repository/Migrations/20190814180425_PrimeiraVersaoBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StarWarPlanets.Repository.Migrations
+{
+    public partial class PrimeiraVersaoBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Planetas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(maxLength: 30, nullable: false),
+                    Clima = table.Column<string>(maxLength: 50, nullable: false),
+                    Terreno = table.Column<string>(maxLength: 200, nullable: false),
+                    QtdAparicao = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Planetas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Planetas");
+        }
+    }
+}
