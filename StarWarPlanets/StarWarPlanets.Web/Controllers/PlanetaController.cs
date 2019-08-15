9af7341a7ed65bb6e1b35f7b59d0d394e8f46675
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2019-08-15 09:52:15 -0300

    Criacao da controller PlanetaController com metodos de consulta e adicionar planeta

---
+++ StarWarPlanets/StarWarPlanets.Web/Controllers/PlanetaController.cs
@@ -0,0 +1,60 @@
+﻿namespace StarWarPlanets.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using StarWarPlanets.Domain.Contratos;
+    using StarWarPlanets.Domain.Entidades;
+    using System;
+
+    namespace QuickBuy.Web.Controllers
+    {
+        [Route("api/[Controller]")]
+        public class PlanetaController : Controller
+        {
+            private readonly IPlanetaRepository _planetaRepository;
+            public PlanetaController(IPlanetaRepository planetaRepository)
+            {
+                _planetaRepository = planetaRepository;
+            }
+
+            /// <summary>
+            /// Retorna todos os planetas
+            /// </summary>
+            /// <returns></returns>  
+            [HttpGet]
+            public IActionResult Get()
+            {
+
+                try
+                {
+                    return Ok(_planetaRepository.ObterTodos());
+
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.ToString());
+                }
+            }
+            /// <summary>
+            /// Insere um novo planeta no banco de dados
+            /// </summary>
+            /// <param name="produto"></param>
+            /// <returns></returns>
+            [HttpPost]
+            public IActionResult Post([FromBody]Planeta planeta)
+            {
+
+                try
+                {
+
+                    _planetaRepository.Adicionar(planeta);
+                    return Created("api/planeta", planeta);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.ToString());
+                }
+
+            }
+        }
+    }
+}
